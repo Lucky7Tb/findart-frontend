b51a6e95ce960bcd572040396e58ea5d6657aee3
Lucky7Tb <Luckytri101@gmail.com>
2021-06-29 14:31:05 +0700

    fix route and middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -4,17 +4,37 @@ export default {
 
 		if (storage == null) {
 			next({ name: 'Login' });
+		} else {
+			next();
 		}
-
-		next();
 	},
 	mustNotAuthenticated(_, from, next) {
 		const storage = localStorage.getItem(process.env.VUE_APP_KEY_STORAGE);
 
 		if (storage !== null) {
 			next(from);
+		} else {
+			next();
 		}
+	},
+	mustArt(_, __, next) {
+		let storage = localStorage.getItem(process.env.VUE_APP_KEY_STORAGE);
+		storage = JSON.parse(storage);
 
-		next();
+		if (storage.role !== 'Art') {
+			next({ name: 'Finder' });
+		} else {
+			next();
+		}
+	},
+	mustArtFinder(_, __, next) {
+		let storage = localStorage.getItem(process.env.VUE_APP_KEY_STORAGE);
+		storage = JSON.parse(storage);
+
+		if (storage.role !== 'Finder') {
+			next({ name: 'Art' });
+		} else {
+			next();
+		}
 	},
 };
