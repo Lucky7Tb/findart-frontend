4ec560b57030de2aa6c13369ae04e3950950c9ba
Lucky7Tb <Luckytri101@gmail.com>
2021-06-29 14:32:48 +0700

    fix

---
+++ src/layouts/finder/FinderNavBar.vue
@@ -0,0 +1,84 @@
+<template>
+	<div>
+		<v-app-bar
+			app
+			color="white"
+			elevation="5"
+			class="rounded-b-xl"
+			height="90"
+		>
+			<v-toolbar-title class="hidden-md-and-up mx-auto">
+				<v-img
+					width="250"
+					lazy-src="@/assets/images/findart_logo_2.png"
+					src="@/assets/images/findart_logo_2.png"
+				></v-img>
+			</v-toolbar-title>
+
+			<v-container class="hidden-sm-and-down">
+				<v-toolbar
+					flat
+					height="90"
+					color="white"
+				>
+					<v-toolbar-title>
+						<v-img
+							lazy-src="@/assets/images/findart_logo_2.png"
+							max-height="250"
+							max-width="250"
+							src="@/assets/images/findart_logo_2.png"
+						></v-img>
+					</v-toolbar-title>
+
+					<v-spacer></v-spacer>
+
+					<v-toolbar-items>
+						<v-btn
+							text
+							plain
+							exact
+							raised
+							depressed
+							exact-active-class="blue--text text--darken-4"
+							v-for="(menu, index) in listMenuFinder"
+							:key="index"
+							:ripple="{
+								class: 'blue--text text--accent-4'
+							}"
+							:to="menu.link"
+						>
+							<v-icon>{{ menu.icon }}</v-icon>
+							<span>{{ menu.name }}</span>
+						</v-btn>
+						<v-btn
+							text
+							plain
+							exact
+							raised
+							depressed
+							:ripple="{
+								class: 'blue--text text--accent-4'
+							}"
+							@click="$emit('navbarLogout')"
+						>
+							<v-icon>mdi-logout</v-icon>
+							<span>Logout</span>
+						</v-btn>
+					</v-toolbar-items>
+				</v-toolbar>
+			</v-container>
+		</v-app-bar>
+	</div>
+</template>
+
+<script>
+import listMenuFinder from '@/router/list-menu-finder';
+
+export default {
+	data() {
+		return {
+			listMenuFinder,
+		};
+	},
+};
+</script>
