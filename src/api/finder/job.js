1955bbd21b45782a62aebff117892c989a8bcff8
Lucky7Tb <Luckytri101@gmail.com>
2021-06-29 23:11:58 +0700

    add job api service

--- src/api/finder/job.js
+++ src/api/finder/job.js
@@ -4,4 +4,20 @@ export default {
 	getJob() {
 		return http.get('api/finder/job');
 	},
+	getDetailJob(idJob) {
+		return http.get(`api/finder/job/${idJob}`);
+	},
+	changeJobThumbnail(idJob, data) {
+		return http.post(
+			`api/finder/job/change-thumbnail/${idJob}`, data,
+			{
+				headers: {
+					'Content-Type': 'application/form-data',
+				},
+			},
+		);
+	},
+	deleteJob(idJob) {
+		return http.delete(`api/finder/job/${idJob}`);
+	},
 };
