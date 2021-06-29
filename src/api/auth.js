aefd6826884d528b1956346422e4420d3fb47f29
Lucky7Tb <Luckytri101@gmail.com>
2021-06-29 23:12:05 +0700

    add content type

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,10 +5,18 @@ export default {
 		return http.get('sanctum/csrf-cookie');
 	},
 	register(data) {
-		return http.post('api/auth/register', data);
+		return http.post('api/auth/register', data, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
 	},
 	login(credentials) {
-		return http.post('api/auth/login', credentials);
+		return http.post('api/auth/login', credentials, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
 	},
 	logout() {
 		return http.post('api/auth/logout');
